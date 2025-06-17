0f6d1b006ffa1ca56137b0d15c6ed08c63706941
infra-astropay <infra+github@astropay.com>
2025-06-17 17:31:41 -0300

    Package.swift added

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Tokenizer",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "Tokenizer",
+            targets: ["Tokenizer"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Tokenizer",
+            url: "https://github.com/infra-astropay/tokenizer-ios-spm/releases/download/1.1.0/Tokenizer.xcframework.zip",
+            checksum: "cb8a4188367a229955b1c8c45f985453bbb33e02785984780cfd244cf1b1d2d3"
+        )
+    ]
+)
