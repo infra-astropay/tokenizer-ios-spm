3aeacaada4abe6ed72f328bb29fc66b87d9c9520
infra-astropay <infra+github@astropay.com>
2025-06-17 17:53:57 -0300

    Update Package.swift to host version 1.1.2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Tokenizer",
-            url: "https://github.com/infra-astropay/tokenizer-ios-spm/releases/download/1.1.0/Tokenizer.xcframework.zip",
+            url: "https://github.com/infra-astropay/tokenizer-ios-spm/releases/download/1.1.2/Tokenizer.xcframework.zip",
             checksum: "cb8a4188367a229955b1c8c45f985453bbb33e02785984780cfd244cf1b1d2d3"
         )
     ]
